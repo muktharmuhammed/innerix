b2d15518ce8c5ce92a20d044eff7ced0250c6968
muktharmuhammed <45305202+muktharmuhammed@users.noreply.github.com>
2022-12-20 15:31:07 +0530

    Delete lib directory

--- lib/resources/color.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class AppColors{
-  static const Color blue= Color(0xFFFF01244C) ;
-  static const Color buttoncolor= Colors.blue ;
-}
